03996b6e26a43a6f90a9b5a71141fbc1a453d6fe
shykhmuhammad07 <muhammadsheikh880@gmail.com>
2025-12-17 16:02:02 +0500

    done

--- backend/index.js
+++ backend/index.js
@@ -2,72 +2,52 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+
 import ReportRoutes from "./Routes/ReportRoutes.js";
 import router from "./Routes/user.Route.js";
 import userAuthenticate from "./Routes/user.authenticate.js";
-import cookieParser from "cookie-parser";
 
 dotenv.config();
-const app = express();
-// const API_URL = process.env.FRONTEND_URL;
-app.use(cookieParser());
-app.use(express.json());
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://health-mate-psi.vercel.app",
-];
+const app = express();
+
+/* ===== Middlewares ===== */
+app.use(express.json());
+app.use(cookieParser());
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      const allowedOrigins = [
-        "http://localhost:5173",
-        "https://health-mate-psi.vercel.app",
-      ];
-      if(allowedOrigins.includes(origin)){
-        callback(null, true);
-      }else{
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: [
+      "http://localhost:5173",
+      "https://health-mate-psi.vercel.app",
+    ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-app.use((req, res, next) => {
-  if (req.method === "OPTIONS") {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
-
-// Routes
+/* ===== Routes ===== */
 app.get("/", (req, res) => {
   res.send("HealthMate API is running...");
 });
+
 app.use("/api/auth", router);
 app.use("/api", userAuthenticate);
 app.use("/api/reports", ReportRoutes);
 
-try {
-  await mongoose.connect(process.env.MONGO_URL);
-  console.log("MongoDB connected");
-} catch (err) {
-  console.error("DB connection failed:", err.message);
-}
+/* ===== DB ===== */
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("Mongo error:", err.message));
 
-console.log("after DB connection");
+/* ===== Local only ===== */
 if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () =>
-    console.log(`server running on http://localhost:${PORT}`)
+    console.log(`Server running on http://localhost:${PORT}`)
   );
 }
 
-// Vercel deployment
+/* ===== Vercel ===== */
 export default app;
